d4094eba92e952a5858c659ef79430b831540ad2
Andrea <andrea.hjelde@gmail.com>
2023-09-13 20:52:21 +0200

    Added content in preference-page

--- app/src/main/java/com/example/mappe1_s364756/Preferences.java
+++ app/src/main/java/com/example/mappe1_s364756/Preferences.java
@@ -1,6 +1,15 @@
 package com.example.mappe1_s364756;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Preferences extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+        Log.d("I siden om preferanser", "onCreate");
+    };
 }
