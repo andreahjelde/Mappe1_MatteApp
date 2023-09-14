3e122ef7c78c44e1665bdb4307a1b3308bf2e1a5
Andrea <andrea.hjelde@gmail.com>
2023-09-14 23:47:47 +0200

    Added content

--- app/src/main/java/com/example/mappe1_s364756/Preferences.java
+++ app/src/main/java/com/example/mappe1_s364756/Preferences.java
@@ -2,14 +2,19 @@ package com.example.mappe1_s364756;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Preferences extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
         Log.d("I siden om preferanser", "onCreate");
     };
+
+
 }
--- app/src/main/java/com/example/mappe1_s364756/StartGame.java
+++ app/src/main/java/com/example/mappe1_s364756/StartGame.java
@@ -1,6 +1,15 @@
 package com.example.mappe1_s364756;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartGame extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startgame);
+        Log.d("I siden StartSpill", "onCreate");
+    };
 }
